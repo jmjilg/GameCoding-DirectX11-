b666996a0f4fafe45b16e50581587a9c6d6727dd
giml790880 <giml790880@gmail.com>
2023-09-20 22:08:08 +0900

    Graphics

--- GameCoding/Game.h
+++ GameCoding/Game.h
@@ -13,15 +13,6 @@ public:
 	void Render();
 
 private:
-	void RenderBegin();
-	void RenderEnd();
-
-private:
-	void CreateDeviceAndSwapChain();
-	void CreateRenderTargetView();
-	void SetViewport();
-
-private:
 	void CreateGeometry();
 	void CreateInputLayout();
 	void CreateVS();
@@ -39,22 +30,12 @@ private:
 
 private:
 	HWND _hwnd;
-	uint32 _width = 0;
-	uint32 _height = 0;
+	//uint32 _width = 0;
+	//uint32 _height = 0;
+	
+	//shared_ptr<Graphics> s;
 
-private:
-	// Device & SwapChain
-	ComPtr<ID3D11Device> _device = nullptr;
-	ComPtr<ID3D11DeviceContext> _deviceContext = nullptr;
-	ComPtr<IDXGISwapChain> _swapChain = nullptr;
-
-
-	// RTV
-	ComPtr<ID3D11RenderTargetView> _renderTargetView;
-
-	// Misc
-	D3D11_VIEWPORT _viewport = { 0 };
-	float _clearColor[4] = { 0.f, 0.f, 0.f, 0.f };
+	Graphics* _graphics;
 
 private:
 	// Geometry
--- GameCoding/pch.h
+++ GameCoding/pch.h
@@ -38,3 +38,6 @@ using namespace Microsoft::WRL;
 #endif
 
 #define CHECK(p)	assert(SUCCEEDED(p))
+
+// Engine
+#include "Graphics.h"
