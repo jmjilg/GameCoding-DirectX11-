ae3eb687edc7eb1d58441098375ff7291d8c7857
giml790880 <giml790880@gmail.com>
2023-10-11 16:09:31 +0900

    Geometry

--- GameCoding/Game.h
+++ GameCoding/Game.h
@@ -30,23 +30,20 @@ private:
 
 private:
 	HWND _hwnd;
-	//uint32 _width = 0;
-	//uint32 _height = 0;
-	
-	//shared_ptr<Graphics> s;
 
-	Graphics* _graphics;
+	shared_ptr<Graphics> _graphics;
 
 private:
 	// Geometry
-	vector<Vertex> _vertices;
-	VertexBuffer* _vertexBuffer;
-	//ComPtr<ID3D11Buffer> _vertexBuffer = nullptr;
+	//vector<Vertex> _vertices;
+	//vector<uint32> _indices;
 
-	vector<uint32> _indices;
-	IndexBuffer* _indexBuffer;
-	InputLayout* _inputLayout;
-	//ComPtr<ID3D11InputLayout> _inputLayout = nullptr;
+	shared_ptr<Geometry<VertexTextureData>> _geometry;
+
+	shared_ptr<VertexBuffer> _vertexBuffer; 
+	shared_ptr<IndexBuffer> _indexBuffer;
+	shared_ptr<InputLayout> _inputLayout;
+
 
 	// VS
 	ComPtr<ID3D11VertexShader> _vertexShader = nullptr;
--- GameCoding/pch.h
+++ GameCoding/pch.h
@@ -6,7 +6,9 @@
 #include "Values.h"
 #include "Struct.h"
 
-// STL
+// STL 
+#include <memory>
+#include <iostream>
 #include <vector>
 #include <list>
 #include <map>
@@ -44,3 +46,6 @@ using namespace Microsoft::WRL;
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
 #include "InputLayout.h"
+#include "Geometry.h"
+#include "GeometryHelper.h"
+#include "VertexData.h"
